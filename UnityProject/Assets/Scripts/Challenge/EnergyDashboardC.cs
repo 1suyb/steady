ed48a54e7f495728772367146f23af14ae33a115
1suyb <suyb1234@gmail.com>
2024-10-15 14:49:02 +0900

    Q1-2
    [구현사항 2] EnergySystemC클래스와 EnergyDashboardC클래스를 완성하여 연료를 사용할 때 발생하는 event Action OnEnergyChanged를 통해 연료바 시스템 구현

--- UnityProject/Assets/Scripts/Challenge/EnergyDashboardC.cs
+++ UnityProject/Assets/Scripts/Challenge/EnergyDashboardC.cs
@@ -5,9 +5,17 @@ public class EnergyDashboardC : MonoBehaviour
 {
     [SerializeField] private EnergySystemC energySystem;
     [SerializeField] private Image fillBar;
-    private void Start()
-    {
-        // 에너지시스템의 에너지 사용에 대해 fillBar가 변경되도록 수정
-    }
+	private void Start()
+	{
+		fillBar = GetComponent<Image>();
+		energySystem.OnEnergyChanged += UpdateDashBoard;
+
+	}
+
+
+	public void UpdateDashBoard(float energy)
+	{
+		fillBar.fillAmount = energy / energySystem.MaxFuel;
+	}
 
 }
